08617779981fddde88cbd2910c0891952f946243
Julian Miranda <jmirandauria@gmail.com>
2022-06-04 23:41:24 -0500

    Owner Promos

--- src/schemas/promotion.schema.ts
+++ src/schemas/promotion.schema.ts
@@ -5,6 +5,7 @@ export const PromotionSchema = new mongoose.Schema(
   {
     status: { type: Boolean, default: true, index: true },
     image: { type: mongoose.Schema.Types.ObjectId, ref: 'Image' },
+    owner: { type: String, default: 'ENCARGA' },
   },
   { ...schemaOptions },
 );
--- src/schemas/promotionFinal.schema.ts
+++ src/schemas/promotionFinal.schema.ts
@@ -6,6 +6,7 @@ export const PromotionFinalSchema = new mongoose.Schema(
     status: { type: Boolean, default: true, index: true },
     image: { type: mongoose.Schema.Types.ObjectId, ref: 'Image' },
     subcategory: { type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory' },
+    owner: { type: String, default: 'ENCARGA' },
   },
   { ...schemaOptions },
 );
